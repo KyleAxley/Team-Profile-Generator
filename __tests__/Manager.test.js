e5e97c05eafba823c0c3e101bb0ee8899b69c123
KyleAxley <kanderson7645@yahoo.com>
2022-08-15 22:45:07 -0400

    manager test pass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,4 @@
-const Employee = require('../library/Manager');
+const Manager = require('../library/Manager');
 
 test('Manager object', () => {
     const manager = new Manager('John', '117', 'MChief@cortana.org', '1170');
@@ -18,9 +18,9 @@ test('Manager office number', () => {
 test('Manager detail', () => {
     const manager = new Manager('John', '117', 'MChief@cortana.org', '1170');
 
-    expect(manager.getRole()).toHaveProperty('John')
-    expect(manager.getRole()).toHaveProperty('117')
-    expect(manager.getRole()).toHaveProperty('MChief@cortana.org')
-    expect(manager.getRole()).toHaveProperty('1170')
+    expect(manager.getRole()).toHaveProperty('name')
+    expect(manager.getRole()).toHaveProperty('id')
+    expect(manager.getRole()).toHaveProperty('email')
+    expect(manager.getRole()).toHaveProperty('officeNumber')
 });
 

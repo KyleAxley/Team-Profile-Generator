5c2632f9a5f8ed4064ea4a733091fbbba433013c
KyleAxley <kanderson7645@yahoo.com>
2022-08-15 23:25:59 -0400

    intern obj/test created

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,25 @@
+const Intern = require('../library/Intern');
+
+test('intern object', () => {
+    const intern = new Intern('John', '117', 'MChief@cortana.org', 'USNC Spartan Project');
+
+    expect(intern.name).toBe('John');
+    expect(intern.id).toBe('117');
+    expect(intern.email).toBe('MChief@cortana.org');
+    expect(intern.school).toBe('USNC Spartan Project');
+});
+
+test('get intern school', () => {
+    const intern = new Intern('John', '117', 'MChief@cortana.org', 'USNC Spartan Project');
+
+    expect(intern.getSchool()).toBe('USNC Spartan Project');
+})
+
+test('intern roles', () => {
+    const intern = new Intern('John', '117', 'MChief@cortana.org', 'USNC Spartan Project');
+
+    expect(intern.getRole()).toHaveProperty('name')
+    expect(intern.getRole()).toHaveProperty('id')
+    expect(intern.getRole()).toHaveProperty('email')
+    expect(intern.getRole()).toHaveProperty('school')
+})
